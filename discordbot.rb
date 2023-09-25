db7bee9bf1a19d9d6698042ee5c5173e1076a3fa
ardririy <hynnkgw27@gmail.com>
2023-09-25 18:20:48 +0900

    フィルタとソートを付けた

--- discordbot.rb
+++ discordbot.rb
@@ -6,17 +6,47 @@ Dotenv.load
 bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
 client = Notion::Client.new(token: ENV['NOTION_API'])
 
-bot.message(with_text: 'Ping!') do |event|
-    event.respond 'Pong!'
+bot.message(with_text: '!ping') do |event|
+    event.respond 'pong!'
 end
 
-bot.message(with_text: '!plan') do |event|
-    # event.respond '1週間後までの予定は以下の通りです:'
-    page = client.database_query(database_id: ENV['DB_ID'])
-        # paginate through all pages
+bot.message(with_text: '!event') do |event|
+    event.respond '1週間後までの予定は以下の通りです:'
+    
+    
+    sorts = [
+        {
+            property: '日付',
+            direction: 'ascending'
+        }
+    ]
+
+    # 今から一週間後までの予定だけを取得する
+    filter = {
+        'and': [
+            {
+                property: '日付',
+        
+                date: {
+                    on_or_after: Date.today,
+                }
+            },
+            {
+                property: '日付',
+                date: {
+                    on_or_before: Date.today + 7,
+                }
+            }
+        ]
+    }
+
+    page = client.database_query(database_id: ENV['DB_ID'], filter: filter, sorts: sorts)
+
     page.results.each do |result|
-        event.respond result.properties['名前'].title[0].plain_text
-        event.respond "------------------"
+        event.respond "."
+        date = result.properties['日付'].date.start
+        name = result.properties['名前'].title[0].plain_text
+        event.respond "#{date}: #{name}"
     end
     
 
